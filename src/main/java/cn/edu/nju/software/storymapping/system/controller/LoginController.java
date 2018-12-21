de7028c541cdff6826917b725b48ced7faae825c
jach <jachly@qq.com>
2018-12-21 18:59:31 +0800

    spring security 前端

--- src/main/java/cn/edu/nju/software/storymapping/system/controller/LoginController.java
+++ src/main/java/cn/edu/nju/software/storymapping/system/controller/LoginController.java
@@ -3,21 +3,22 @@ package cn.edu.nju.software.storymapping.system.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class LoginController {
 
     // Login form
-    @RequestMapping("/login.html")
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
     public String login() {
-        return "login.html";
+        return "login";
     }
 
     // Login form with error
-    @RequestMapping("/login-error.html")
+    @RequestMapping(value = "/login-error", method = RequestMethod.GET)
     public String loginError(Model model) {
         model.addAttribute("loginError", true);
-        return "login.html";
+        return "login";
     }
 
 }
--- src/main/java/cn/edu/nju/software/storymapping/system/controller/UserController.java
+++ src/main/java/cn/edu/nju/software/storymapping/system/controller/UserController.java
@@ -3,10 +3,10 @@ package cn.edu.nju.software.storymapping.system.controller;
 import cn.edu.nju.software.storymapping.system.entity.User;
 import cn.edu.nju.software.storymapping.system.service.UserService;
 import cn.edu.nju.software.storymapping.utils.UserUtil;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/sys")
@@ -15,10 +15,23 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-    @RequestMapping("/getUser")
+    @GetMapping("/getUser")
     public User GetUser(){
         //MyUserDetails user = (MyUserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         return userService.queryByUsername(UserUtil.currentUserName());
+    }
+
+    /**
+     * 根据id查询用户
+     *
+     * @param id
+     * @return
+     */
+    @GetMapping("/users/{id}")
+    @ApiOperation(value = "获取用户详细信息", notes = "根据url的id来获取用户详细信息")
+    @ApiImplicitParam(name = "id", value = "用户ID", required = true, dataType = "Long", paramType = "path")
+    public String findById(@PathVariable Long id) {
+        return "";
     }
 
     /**
