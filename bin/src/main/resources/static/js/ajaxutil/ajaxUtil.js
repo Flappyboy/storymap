f623a5c5e83cdcbb8f63295ca4aa07f6ed39741b
jach <jachly@qq.com>
2018-12-22 16:16:50 +0800

    删除bin

--- bin/src/main/resources/static/js/ajaxutil/ajaxUtil.js
+++
@@ -1,9 +0,0 @@
-$(function () {
-
-    $.fn.ajaxUtil = function(options){
-        var opts = $.extend({}, $.fn.ajaxUtil.defaults, options);
-        $.post(opts.url,opts.data,opts.fun);
-    }
-
-    $.fn.ajaxUtil.defaults = {}
-})
