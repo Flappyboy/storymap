a0299b0fa1f82e2db0ef0634b00141ade5990942
jach <jachly@qq.com>
2019-01-11 00:19:40 +0800

    增加card编辑功能

--- src/main/resources/static/js/story-map.js
+++ src/main/resources/static/js/story-map.js
@@ -1,4 +1,27 @@
+
+function addActivity(){
+    addHead();
+    addContent();
+}
+
 $(function() {
-    $( ".board-subtasks" ).sortable();
-    $( ".board-tasks" ).sortable();
+    $( ".board-subtasks" ).sortable({
+        connectWith: ".ui-sortable"
+    });
+    $( ".board-tasks" ).sortable({
+        connectWith: ".ui-sortable"
+    });
+    $(".board-card-title-text").click(function(){
+        var textarea = $(this).siblings("textarea");
+        var textBoard = $(this);
+        textarea.val(textBoard.text());
+        textBoard.hide();
+        textarea.show();
+        textarea.blur(function(){
+            textBoard.show();
+            textBoard.text($(this).val());
+            $(this).hide();
+        });
+        textarea.focus();
+    });
 });
