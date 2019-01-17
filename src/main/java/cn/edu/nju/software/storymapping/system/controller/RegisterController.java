d69fa968e792806bf94b01f0dc477e7cbd2e357c
jach <jachly@qq.com>
2019-01-17 12:47:14 +0800

    修复注册bug

--- src/main/java/cn/edu/nju/software/storymapping/system/controller/RegisterController.java
+++ src/main/java/cn/edu/nju/software/storymapping/system/controller/RegisterController.java
@@ -11,14 +11,16 @@ import cn.edu.nju.software.storymapping.system.dto.Response;
 import cn.edu.nju.software.storymapping.system.entity.User;
 import cn.edu.nju.software.storymapping.system.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Date;
 
-@RestController
+@Controller
 public class RegisterController {
     @Autowired
     private UserService userService;
@@ -32,16 +34,25 @@ public class RegisterController {
     @Autowired
     private ActivityCardService activityCardService;
 
-    @PostMapping("/pub/api/register")
-    public Response register(User user) {
+    @GetMapping("/register")
+    public String registerPage(){
+        return "register";
+    }
+
+    @PostMapping("/register")
+    public String register(User user) {
+        String error=null;
         if (StringUtils.isEmpty(user.getUsername())) {
-            return Response.createDefaultResponse().fail("用户名不能为空！");
+            error="用户名不能为空！";
         }
         if (StringUtils.isEmpty(user.getPassword())) {
-            return Response.createDefaultResponse().fail("密码不能为空！");
+            error="密码不能为空！";
         }
         if (containUser(user.getUsername())) {
-            return Response.createDefaultResponse().fail("用户名已存在！");
+            error="用户名已存在！";
+        }
+        if(error!=null) {
+            return "register";
         }
         //创建角色
         user.setCreateTime(new Date());
@@ -49,10 +60,11 @@ public class RegisterController {
         userService.insertUser(user);
         //初始化一个workspace
         initWorkSpace(user.getId());
-        return Response.createDefaultResponse().success("注册成功！");
+        return "login";
     }
 
     @GetMapping("/pub/api/validation")
+    @ResponseBody
     public Response validation(String username) {
         if (username == null || "".equals(username.trim()))
             return Response.createDefaultResponse().fail("用户名不能为空！");
